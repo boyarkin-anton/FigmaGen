9e3caad8f8645f13da551076968417680a12677f
Anton Boyarkin <boyarkin.anton@gmail.com>
2021-04-17 11:40:58 +0300

    Update Package dependensies version

--- Package.swift
+++ Package.swift
@@ -13,13 +13,13 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/jakeheis/SwiftCLI", from: "5.0.0"),
-        .package(url: "https://github.com/onevcat/Rainbow.git", from: "3.0.0"),
-        .package(url: "https://github.com/jpsim/Yams.git", from: "2.0.0"),
-        .package(url: "https://github.com/kylef/PathKit.git", from: "0.9.2"),
+        .package(url: "https://github.com/jakeheis/SwiftCLI", from: "6.0.0"),
+        .package(url: "https://github.com/onevcat/Rainbow.git", from: "4.0.0"),
+        .package(url: "https://github.com/jpsim/Yams.git", from: "4.0.0"),
+        .package(url: "https://github.com/kylef/PathKit.git", from: "1.0.0"),
         .package(url: "https://github.com/kylef/Stencil.git", from: "0.13.0"),
         .package(url: "https://github.com/SwiftGen/StencilSwiftKit.git", from: "2.7.2"),
-        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0-rc.2"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.4.0"),
         .package(url: "https://github.com/mxcl/PromiseKit.git", from: "6.8.0")
     ],
     targets: [
